d59d4ca0e528840c2c38e5f583d91fde066f23c5
octanym <cmrnmr@gmail.com>
2023-01-25 14:59:02 -0500

    game function tests passing

--- index.js
+++ index.js
@@ -193,9 +193,33 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
+let computer = Math.random();
+
+if (computer <= 0.33) {
+  computer = 'rock';
+} else if (computer > 0.33 && computer < 0.66) {
+  computer = 'paper';
+} else if (computer > 0.66) {
+  computer = 'scissors';
+}
 
 function game(user, computer) {
   /*add your code here*/
+  if (user == computer) {
+    return "it's a tie";
+  } else if (user == 'rock' && computer == 'paper') {
+    return 'you lose!';
+  } else if (user == 'rock' && computer == 'scissors') {
+    return 'you win!';
+  } else if (user == 'paper' && computer == 'rock') {
+    return 'you win!';
+  } else if (user == 'paper' && computer == 'scissors') {
+    return 'you lose!';
+  } else if (user == 'scissors' && computer == 'rock') {
+    return 'you lose!';
+  } else if (user == 'scissors' && computer == 'paper') {
+    return 'you win!';
+  }
 }
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
